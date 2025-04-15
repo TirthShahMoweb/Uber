a65ec4f5ca1dccc2b4ca234b70f01cf1efa46cf8
TirthShahMoweb <tirth.web.moweb@gmail.com>
2025-04-15 18:42:23 +0530

    Fix : In requirement.txt fileand in Vehicle view and serializers

--- vehicle/serializers/vehicleSerializers.py
+++ vehicle/serializers/vehicleSerializers.py
@@ -1,66 +1,66 @@
-from django.urls import reverse
+# from django.urls import reverse
 
-from rest_framework import serializers
+# from rest_framework import serializers
 
-from ..models import Vehicle, VehicleRequest
-from user.models import DriverDetail
-
-
-
-class VehicleSerializer(serializers.ModelSerializer):
-    vehicle_front_image = serializers.ImageField(required=True)
-    vehicle_back_image = serializers.ImageField(required=True)
-    vehicle_leftSide_image = serializers.ImageField(required=True)
-    vehicle_rightSide_image = serializers.ImageField(required=True)
-    vehicle_rc_front_image = serializers.ImageField(required=True)
-    vehicle_rc_back_image = serializers.ImageField(required=True)
-
-    class Meta:
-        model = VehicleRequest
-        fields = ('vehicle_number', 'vehicle_type', 'vehicle_type',)
-
-    def create(self, validated_data):
-        user = self.context['user']
-        try:
-            driver = DriverDetail.objects.get(user=user)
-        except DriverDetail.DoesNotExist:
-            raise serializers.ValidationError("Driver details not found for this user.")
+# from ..models import Vehicle, VehicleRequest
+# from user.models import DriverDetail
 
 
-        vehicle = VehicleRequest.objects.create(driver=driver, **validated_data)
-        return vehicle
+
+# class VehicleSerializer(serializers.ModelSerializer):
+#     vehicle_front_image = serializers.ImageField(required=True)
+#     vehicle_back_image = serializers.ImageField(required=True)
+#     vehicle_leftSide_image = serializers.ImageField(required=True)
+#     vehicle_rightSide_image = serializers.ImageField(required=True)
+#     vehicle_rc_front_image = serializers.ImageField(required=True)
+#     vehicle_rc_back_image = serializers.ImageField(required=True)
+
+#     class Meta:
+#         model = VehicleRequest
+#         fields = ('vehicle_number', 'vehicle_type', 'vehicle_type',)
+
+#     def create(self, validated_data):
+#         user = self.context['user']
+#         try:
+#             driver = DriverDetail.objects.get(user=user)
+#         except DriverDetail.DoesNotExist:
+#             raise serializers.ValidationError("Driver details not found for this user.")
 
 
-class VehicleVerificationPendingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        fields = ('id', 'vehicle_image', 'vehicle_rc', 'vehicle_type',)
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['approval_url'] = reverse('vehicle-verification-approval', args=[instance.id])
-        return representation
+#         vehicle = VehicleRequest.objects.create(driver=driver, **validated_data)
+#         return vehicle
 
 
-class ResubmissionVehicleSeralizer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        fields = ('vehicle_image', 'vehicle_rc', 'vehicle_type',)
+# class VehicleVerificationPendingSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Vehicle
+#         fields = ('id', 'vehicle_image', 'vehicle_rc', 'vehicle_type',)
 
-class ResubmissionVehicleSeralizer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        fields = ('vehicle_image', 'vehicle_rc', 'vehicle_type',)
+#     def to_representation(self, instance):
+#         representation = super().to_representation(instance)
+#         representation['approval_url'] = reverse('vehicle-verification-approval', args=[instance.id])
+#         return representation
 
 
-class DisplayVehicleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        # fields = '__all__'
-        fields = ('id','driver','vehicle_image', 'vehicle_rc', 'vehicle_type',)
+# class ResubmissionVehicleSeralizer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Vehicle
+#         fields = ('vehicle_image', 'vehicle_rc', 'vehicle_type',)
 
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     request = self.context['request']
-    #     representation['vehicle_url'] = reverse('Select_vehicle', args=[instance.id])
-    #     return representation
+# class ResubmissionVehicleSeralizer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Vehicle
+#         fields = ('vehicle_image', 'vehicle_rc', 'vehicle_type',)
+
+
+# class DisplayVehicleSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Vehicle
+#         # fields = '__all__'
+#         fields = ('id','driver','vehicle_image', 'vehicle_rc', 'vehicle_type',)
+
+#     # def to_representation(self, instance):
+#     #     representation = super().to_representation(instance)
+#     #     request = self.context['request']
+#     #     representation['vehicle_url'] = reverse('Select_vehicle', args=[instance.id])
+#     #     return representation
